16f71f69c4315ef15fee52c0c19460353ce6c0e5
Devil-mYcRy <51002209+Devil-mYcRy@users.noreply.github.com>
2021-01-29 16:24:22 +0300

    Вынесены методы загрузки и сохранения

---
+++ FintnessAppBusinessLogic/Control/BaseController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintnessAppBusinessLogic.Control
+{
+    public abstract class BaseController
+    {
+        /// <summary>
+        /// Сохранение в файл.
+        /// </summary>
+        /// <param name="fileName"> Название файла. </param>
+        /// <param name="item"> Сохроняемый объект. </param>
+        protected void Save(string fileName, object item)
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, item);
+            }
+        }
+        protected T Load<T>(string fileName)
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                if (fs.Length > 0 && formatter.Deserialize(fs) is T item)
+                {
+                    return item;
+                }
+                else
+                {
+                    return default(T);
+                }
+               
+            }
+        }
+    }
+}
