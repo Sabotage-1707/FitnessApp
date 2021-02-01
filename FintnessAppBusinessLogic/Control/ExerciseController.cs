b0db9b468ca3d7ad3d04d650e0f2026bd7fd5e71
Devil-mYcRy <51002209+Devil-mYcRy@users.noreply.github.com>
2021-02-01 22:27:56 +0300

    Реализована связь с бд

---
+++ FintnessAppBusinessLogic/Control/ExerciseController.cs
@@ -0,0 +1,61 @@
+﻿using FintnessAppBusinessLogic.Model;
+using FitnessAppBusinessLogic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintnessAppBusinessLogic.Control
+{
+    public class ExerciseController : BaseController
+    {
+        private readonly User user;
+        public List<Exercise> Exercises { get; }
+        public List<Activity> Activities { get; }
+        private IDataSaver manager;
+
+        public ExerciseController(User user, IDataSaver manager)
+        {
+            this.user = user ?? throw new ArgumentNullException(nameof(user));
+            this.manager = manager ?? throw new ArgumentNullException("Способ работы не может быть пустым или null", nameof(manager));
+            Exercises = GetAllExercises();
+            Activities = GetAllActivities();
+        }
+
+        private List<Activity> GetAllActivities()
+        {
+            return Load<Activity>(manager) ?? new List<Activity>();
+        }
+
+        public void Add(Activity activity, DateTime begin, DateTime end)
+        {
+            var act = Activities.SingleOrDefault(a => a.Name == activity.Name);
+
+            if (act == null)
+            {
+                Activities.Add(activity);
+
+                var exercise = new Exercise(begin, end, activity, user);
+                Exercises.Add(exercise);
+            }
+            else
+            {
+                var exercise = new Exercise(begin, end, act, user);
+                Exercises.Add(exercise);
+            }
+            Save();
+        }
+
+        private List<Exercise> GetAllExercises()
+        {
+            return Load<Exercise>(manager) ?? new List<Exercise>();
+        }
+
+        private void Save()
+        {
+            Save(Exercises, manager);
+            Save(Activities, manager);
+        }
+    }
+}
---
+++ FintnessAppBusinessLogic/Control/FitnessContext.cs
@@ -0,0 +1,19 @@
+﻿using FintnessAppBusinessLogic.Model;
+using FitnessAppBusinessLogic.Model;
+using System.Data.Entity;
+
+
+namespace FintnessAppBusinessLogic.Control
+{
+    public class FitnessContext : DbContext
+    {
+        public FitnessContext() : base("DbConnection") { }
+
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Eating> Eatings { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
